c682e1a56f74bf0f7d493b6b8dddfb02cb4ffd32
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-13 21:43:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,11 @@
 sentence = ["Hello there, and how are you?"]
 
 def reverse_each_word(sentence)
-  sentence_array = sentence.split
-  sentence_array.collect do |words|
+  sentence = sentence.split
+  sentence.collect do |words|
     words.reverse
   end.join(" ")
-  sentence_array.each do |words|
+  sentence.each do |words|
     "#{words.reverse}"
   end
 end
