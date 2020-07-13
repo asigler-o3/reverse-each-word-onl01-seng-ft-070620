9cdf502972cc5e2dd0cd40341bd65fcb8dec54d0
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-13 21:43:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,6 @@ def reverse_each_word(sentence)
   sentence = sentence.split
   sentence.collect do |words|
     words.reverse
-  end.join(" ")
   sentence.each do |words|
     "#{words.reverse}"
   end
